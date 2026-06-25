69368058958ad2b4a7455611eeaf7a06dbdd8f9d
KangyuX <155896620+KangyuX@users.noreply.github.com>
2026-06-25 11:40:32 -0400

    Prettier Now

--- app/read_the_bible/api/route.ts
+++ app/read_the_bible/api/route.ts
@@ -1,15 +1,24 @@
 export async function GET() {
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: "SECRET_KEY",
+      Accept: "application/json",
+    },
+  };
 
-    const options = {
-  method: 'GET',
-  headers: {
-    'Authorization': 'SECRET_KEY',
-    'Accept': 'application/json'
+  try {
+    const res = await fetch(
+      "https://api.youversion.com/v1/bibles/3034/passages/MAT.1",
+      options
+    );
+
+    const data = await res.json();
+    console.log(data);
+
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
-};
-
-  return fetch('https://api.youversion.com/v1/bibles/3034/passages/MAT.1', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
 }
