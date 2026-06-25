6177da3611bbae62699b5ffce0b009de1bfdd443
KangyuX <155896620+KangyuX@users.noreply.github.com>
2026-06-25 10:54:07 -0400

    edit api/route.ts

--- app/read_the_bible/api/route.ts
+++ app/read_the_bible/api/route.ts
@@ -1,1 +1,15 @@
+export async function fetchBiblePassage() {
 
+    const options = {
+  method: 'GET',
+  headers: {
+    'Authorization': 'SECRET_KEY',
+    'Accept': 'application/json'
+  }
+};
+
+  return fetch('https://api.youversion.com/v1/bibles/3034/passages/MAT.1', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+}
